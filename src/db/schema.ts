e38a9aec88ad22c40f5bc582338de7b1d90c8ef3
DFCU\stuart.smith <stuarts205@gmail.com>
2026-02-20 13:37:23 -0700

    02: database

---
+++ src/db/schema.ts
@@ -0,0 +1,18 @@
+import { pgTable, text, timestamp, boolean, pgEnum } from "drizzle-orm/pg-core";
+import { nanoid } from "nanoid";
+
+export const users = pgTable("users", {
+  id: text("id").primaryKey(),
+  name: text("name").notNull(),
+  email: text("email").notNull().unique(),
+  emailVerified: boolean("email_verified")
+    .$defaultFn(() => false)
+    .notNull(),
+  image: text("image"),
+  createdAt: timestamp("created_at")
+    .$defaultFn(() => /* @__PURE__ */ new Date())
+    .notNull(),
+  updatedAt: timestamp("updated_at")
+    .$defaultFn(() => /* @__PURE__ */ new Date())
+    .notNull(),
+});
